961727c946e9a78353c0103cfa92f8872caaac23
azzuwan aziz <azzuwan@gmail.com>
2016-10-07 04:51:42 +0800

    Add simple test for GPS device

--- gt.py
+++ gt.py
@@ -1,7 +1,14 @@
 #! /usr/bin/env python
 # License: MIT
  
-import os
+import os, time
 from gps import *
 
-gpsd = gps.gps(mode=WATCH_ENABLE)
+
+gpsd = gps(mode=WATCH_ENABLE)
+
+for x in range(0, 30):
+  gpsd.next()
+  print gpsd.fix.latitude, ", ", gpsd.fix.longitude
+  time.sleep(1.5)
+
